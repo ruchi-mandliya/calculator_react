bfda429dbeef832494b0140a6703a2fe0cd1a1b4
Ruchi Mandliya <rmandliya18@gmail.com>
2023-08-23 11:21:05 +0530

    add functionalty on buttons

--- cls/src/components/Calculator.jsx
+++ cls/src/components/Calculator.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import { useState } from "react";
 
 function Calculator() {
+  const [value, setValue] = useState("");
+
+  const clearDisplay = () => {
+    setValue("");
+  };
+
+  const deleteValue = () => {
+    setValue(value.substring(0, value.length - 1));
+  };
+
+  const calculate = () => {
+    setValue(eval(value).toString());
+  };
+
   return (
     <div className="bg-blue-900 text-white w-screen h-screen flex justify-center items-center">
       <div className=" bg-gray-900 w-96 rounded-xl shadow-xl border-2 border-gray-50">
-        {/* Screen  */}
+        {/* display for showing values  */}
         <div className="p-10 ">
           <input
             type="text"
             className="px-1.5 text-right placeholder-black outline-none rounded-lg bg-gray-400 text-2xl pt-20 w-full text-black"
             placeholder="0"
+            value={value}
           />
         </div>
 
@@ -20,21 +35,25 @@ function Calculator() {
               type="button"
               value="C"
               className="bg-red-500 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={clearDisplay}
             />
             <input
               type="button"
               value="del"
               className="bg-green-500 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={deleteValue}
             />
             <input
               type="button"
               value="%"
               className="bg-blue-500 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="/"
               className="bg-orange-300 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
           </div>
 
@@ -43,21 +62,25 @@ function Calculator() {
               type="button"
               value="7"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="8"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="9"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="*"
               className="bg-pink-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
           </div>
 
@@ -66,21 +89,25 @@ function Calculator() {
               type="button"
               value="4"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="5"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="6"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="-"
               className="bg-indigo-300 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
           </div>
 
@@ -89,21 +116,25 @@ function Calculator() {
               type="button"
               value="1"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="2"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="3"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="+"
               className="bg-sky-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
           </div>
 
@@ -112,21 +143,25 @@ function Calculator() {
               type="button"
               value="0"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="00"
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="."
               className="bg-gray-200 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={(e) => setValue(value + e.target.value)}
             />
             <input
               type="button"
               value="="
               className="bg-green-300 shadow-md rounded-lg w-12 h-12 text-center text-black font-medium flex justify-center items-center outline-none"
+              onClick={calculate}
             />
           </div>
         </div>
